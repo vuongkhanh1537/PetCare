ced29c4d962dccfefe0ae6bd3c3d6e1f8f80aa6a
VuongKhanh(Kal) <122599235+vuongkhanh1537@users.noreply.github.com>
2023-11-12 17:12:47 +0700

    Implement logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
-import {Routes, Route, useNavigate} from 'react-router-dom'
+import {Routes, Route, useNavigate, AuthProvider} from 'react-router-dom'
 import Home from './global/homePage/Home';
 import Employee from './admin/page/employee/Employee';
 import Login from './global/login/Login';
 import Dashboard from './admin/page/dashboard/Dashboard'
 import Product from './admin/page/product/Product';
-import Service from './admin/page/service/Service'
- 
+import Service from './admin/page/service/Service';
+import { useState } from 'react';
+
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}  />
-      <Route path="/login" element={<Login/>}  />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path="/san_pham" element={<Product />} />
-      <Route path="/nhan_vien" element={<Employee/>} />
-      <Route path="/dich_vu" element={<Service />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home/>}  />
+        <Route path="/login" element={<Login/>}  />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/san_pham" element={<Product />} />
+        <Route path="/nhan_vien" element={<Employee/>} />
+        <Route path="/dich_vu" element={<Service />} />
+      </Routes>
   );
 }
 
--- client/src/admin/page/global/Topbar.jsx
+++ client/src/admin/page/global/Topbar.jsx
@@ -1,17 +1,45 @@
 import {Box, IconButton} from "@mui/material"
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import * as React from 'react';
+import { useNavigate } from "react-router-dom";
 
 
 const Topbar = () => {
+    const MenuPopupState = () => {
+        const navigate = useNavigate();
+
+        const handleLogout = () => {
+            localStorage.removeItem("token");
+            navigate("/");
+        }
+
+        return (
+          <PopupState variant="popover" popupId="demo-popup-menu">
+            {(popupState) => (
+              <React.Fragment>
+                <IconButton {...bindTrigger(popupState)}>
+                    <PersonOutlinedIcon></PersonOutlinedIcon>
+                </IconButton> 
+                <Menu {...bindMenu(popupState)}>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
+              </React.Fragment>
+            )}
+          </PopupState>
+        );
+      }
+
     return (
     <Box display="flex" justifyContent="right" p={2}>
         <IconButton>
             <NotificationsOutlinedIcon></NotificationsOutlinedIcon>
         </IconButton>
-        <IconButton>
-            <PersonOutlinedIcon></PersonOutlinedIcon>
-        </IconButton> 
+        <MenuPopupState />
     </Box>
     )
 }
