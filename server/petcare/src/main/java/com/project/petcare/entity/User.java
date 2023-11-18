d4a885b0cf295d6cddbb15114becf23e5f566fc4
Tuan <tuan.kieudangquoc03@hcmut.edu.vn>
2023-11-18 17:52:38 +0700

    login module

---
+++ server/petcare/src/main/java/com/project/petcare/entity/User.java
@@ -0,0 +1,33 @@
+package com.project.petcare.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username",nullable = false, unique = true, length = 20)
+    private String username;
+
+    @Column(name = "password",nullable = false, length = 20)
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.role = "guest";
+    }
+
+}
