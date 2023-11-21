1b8af68894a8a2d93c95fc416078ed7a658b7450
NDHoang2003 <hoang03fanstar@gmail.com>
2023-11-21 21:23:41 +0700

    add product module

---
+++ server/admin/admin/src/main/java/com/example/admin/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.admin.repository;
+
+import com.example.admin.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAll();
+    Optional<Product> findById(Long productId);
+    List<Product> findByProductName(String productName);
+
+}
